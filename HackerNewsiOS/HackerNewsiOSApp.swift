acc90cd62217860c1dd3f39c197c4669bafc6a69
mattwbdv <koenigmattd@gmail.com>
2023-05-25 16:54:44 -0700

    Initial Commit

---
+++ HackerNewsiOS/HackerNewsiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsiOSApp.swift
+//  HackerNewsiOS
+//
+//  Created by Matt Koenig on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
